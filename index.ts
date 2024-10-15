b19b74bbd0a178cd0a8be198abe4c934e24cef6a
Mr Coxall <mr.coxall@mths.ca>
2024-10-15 12:11:01 -0400

    output fix

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@
  * version 1.0
  * since   2020-01-01
  */
-import {Bicycle} from './Bicycle.ts'
+import { Bicycle } from './Bicycle.ts'
 
 // use Bicycle class
 const someBicycle1 = new Bicycle()
@@ -17,7 +17,7 @@ console.log(`The current speed on bike 1 is: ${someBicycle1.speed}`)
 
 console.log('\nSet speed to 10 on bike 2.')
 someBicycle2.speed = 5
-console.log(`The current speed on bike 2 is:' ${someBicycle2.speed}`)
+console.log(`The current speed on bike 2 is: ${someBicycle2.speed}`)
 
 console.log('\nApply the brake with value of 2.')
 someBicycle1.applyBrake(2)
