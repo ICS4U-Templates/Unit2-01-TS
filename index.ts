151caf12ce0053f6f2fe30ef897947f598f54e58
Patrick Coxall <mr.coxall@mths.ca>
2024-10-15 11:36:00 -0400

    init

---
+++ index.ts
@@ -0,0 +1,29 @@
+/**
+ * This is the standard index.ts starting point.
+ *
+ * author  Mr Coxall
+ * version 1.0
+ * since   2020-01-01
+ */
+import MyBicycle from './Bicycle.ts'
+
+// use Bicycle class
+const someBicycle1 = new MyBicycle()
+const someBicycle2 = new MyBicycle()
+someBicycle1.
+
+console.log('\nSet speed to 10 on bike 1.')
+someBicycle1.speed = 10
+console.log(`The current speed on bike 1 is: ${someBicycle1.speed}`)
+
+console.log('\nSet speed to 10 on bike 2.')
+someBicycle2.speed = 5
+console.log(`The current speed on bike 2 is:' ${someBicycle2.speed}`)
+
+console.log('\nApply the brake with value of 2.')
+someBicycle1.applyBrake(2)
+someBicycle2.applyBrake(2)
+console.log(`The new speed on bike 1 is: ${someBicycle1.speed}`)
+console.log(`The new speed on bike 2 is: ${someBicycle2.speed}`)
+
+console.log('\nDone.')
